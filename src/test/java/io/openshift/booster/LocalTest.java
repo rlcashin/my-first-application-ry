120b1cb3c6be5b795d3af7ffd8dee1c1abbe7fc0
Ryan <rlcashin@hotmail.com>
2018-08-31 13:10:41 +0000

    test12

--- src/test/java/io/openshift/booster/LocalTest.java
+++ src/test/java/io/openshift/booster/LocalTest.java
@@ -20,8 +20,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LocalTest extends AbstractBoosterApplicationTest {
 
     @Value("${local.server.port}")
